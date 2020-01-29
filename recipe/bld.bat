139e568ab11a46b3d9b8bb2f9fdd05510b5cf3bd
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-01-29 11:48:40 -0700

    Try using NMake
    ... following https://github.com/conda-forge/jpeg-feedstock/blob/master/recipe/bld.bat#L15 and based on my vague understanding of similar issues elsewhere.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,7 +8,7 @@ cd build
 set CMAKE_FLAGS=-DCMAKE_INSTALL_PREFIX=%PREFIX%
 set CMAKE_FLAGS=%CMAKE_FLAGS% -DCMAKE_BUILD_TYPE=Release
 
-cmake %CMAKE_FLAGS% ..
+cmake -G "NMake Makefiles" %CMAKE_FLAGS% ..
 if errorlevel 1 exit /b 1
 
 cmake --build . --config Release --target install
