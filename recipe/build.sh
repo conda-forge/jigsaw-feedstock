954aadb29a0a1839c08f7b2ac5c6acc6eb41ca63
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-02-13 21:57:10 -0700

    Don't test when cross-compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,12 +11,14 @@ cmake --build . --config Release --target install
 cd ..
 
 # unit tests
-cd uni
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build . --config Debug
-for test in 1 2 3 4 5 6 7 8 9
-do
-  ./test_${test}
-done
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+  cd uni
+  mkdir build
+  cd build
+  cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Debug ..
+  cmake --build . --config Debug
+  for test in 1 2 3 4 5 6 7 8 9
+  do
+    ./test_${test}
+  done
+fi
